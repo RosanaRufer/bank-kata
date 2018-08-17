d7422679890f6776e9e0e0c37e188f2efa9073ed
Rosana Ruiz <rosanarufer@gmail.com>
2018-08-17 13:28:22 +0100

    Refactor: Extract reusable lines and group in describe block

--- src/bank-account.spec.js
+++ src/bank-account.spec.js
@@ -2,18 +2,25 @@ const { Account } = require("./bank-account");
 
 describe('BankAccount', () => {
 
+    describe('When first created', () => {
 
-    it('Should return no statements when account is new', () => {
-        const account = new Account();
-        const result = account.statementsCount();
-        expect(result).toBe(0)
-    });
+        let account;
 
-    it('Should have one statement after first deposit', () => {
-        const account = new Account();
-        account.makeDeposit(100);
-        const result = account.statementsCount();
-        expect(result).toBe(1);
+        beforeEach( () => {
+            account = new Account();
+        });
+
+        it('Should return no statements when account is new', () => {
+            const result = account.statementsCount();
+            expect(result).toBe(0)
+        });
+
+        it('Should have one statement after first deposit', () => {
+            account.makeDeposit(100);
+            const result = account.statementsCount();
+            expect(result).toBe(1);
+        });
+
     });
 
 });
